f8c068aa27d5d246fa0879e21aa4c4788f71053f
Zaria-nob <zarianobilo@rhs.school.nz>
2024-11-04 15:02:55 +1300

    Add files via upload
    I've added one line of code for turning off the relay once it has gone down to 79% humidity. 
    I have commented my code.

--- Greenhouse_interrupt.ino
+++ Greenhouse_interrupt.ino
@@ -1,44 +1,53 @@
+//includes the libraries needed
 #include <DHT11.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-int temperature = 0;
+// creating variables for temp and humidity
+int temperature = 0;  
 int humidity = 0;
 
-unsigned long lastButton = 0;
-const int interruptPin = 2;
 
-DHT11 dht11(6);
+unsigned long lastButton = 0;  //create variable for the button timer. This variable cannot have negative numbers, and can hold a lot of data.
+const int interruptPin = 2;   //makes the variable for the pin that is connected to the interrupt. Is read-only.
 
+DHT11 dht11(6);  //makes pin for DHT11 sensor
+
+
+//function for the interrupt
 void InterruptFunction() {
-    lastButton = millis();
+    lastButton = millis();  //makes the variable lastButton == the function millis()
 }
 
+
+//function to set up everything, runs once.
 void setup() {
   // put your setup code here, to run once:
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), InterruptFunction, FALLING);
   
-  Serial.begin(9600);
+  pinMode(interruptPin, INPUT_PULLUP);     //makes inerruptPin an input
+  attachInterrupt(digitalPinToInterrupt(interruptPin), InterruptFunction, FALLING);  //sets up the interrupt with all the settings (digitalPinToInterrupt(pin), function, mode)
   
-  pinMode(4, OUTPUT);
+  Serial.begin(9600);      // sets the data rate in baud(bits per second)
   
-  lcd.init();                      // initialize the lcd 
-  lcd.backlight();
+  pinMode(4, OUTPUT);      // makes pin 4 an output
+  
+  lcd.init();              // initialize the lcd 
+  lcd.backlight();         // turns on the backlight
   
 }
 
+
+//the main function that continously loops
 void loop() {
   
     // Attempt to read the temperature and humidity values from the DHT11 sensor.
     int result = dht11.readTemperatureHumidity(temperature, humidity);
-    lcd.noBacklight();
+    lcd.noBacklight();  //turns off the backlight
     
     // Check the results of the readings.
     // If the reading is successful, print the temperature and humidity values.
-    // If there are errors, print the appropriate error messages.
     if (result == 0) {
         //reads the temp and humidity
         Serial.print("Temperature: ");
@@ -47,7 +56,7 @@ void loop() {
         Serial.print(humidity);
         Serial.println(" %");
 
-        // Print a message to the LCD.
+        // Prints the temp and humidity on the LCD.
         lcd.setCursor(0,0);
         lcd.print("Temp: ");
         lcd.print(temperature);
@@ -57,22 +66,26 @@ void loop() {
         lcd.print("Humidity: ");
         lcd.print(humidity);
         lcd.print("% ");
-
+        
+// If there are errors, print the appropriate error messages.
     } else {
         // Print error message based on the error code.
         Serial.println(DHT11::getErrorString(result));
     }
 
-    
-    if(lastButton+5000 > millis()){
-      lcd.backlight();
+    //if the button was pressed in the last 5 seconds it runs the code
+    if(lastButton+5000 > millis()){ 
+      lcd.backlight(); //turns on backlight
     }
     else{
-      lcd.noBacklight();
+      lcd.noBacklight(); //turns off backlight
     }
 
     
     if (humidity > 79) {
       digitalWrite(4, HIGH); //turns on relay
+    }
+    else{
+      digitalWrite(4,LOW); //turns off relay
     }
 }
